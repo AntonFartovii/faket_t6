10066015297c9272c0b1ad058d0cba45afc7dd7b
AntonFartovii <anton@rza.by>
2022-09-29 13:20:18 +0300

    feat: express

---
+++ app/app.js
@@ -0,0 +1,4 @@
+console.log('app.js')
+const app = document.querySelector('.app')
+
+
--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 import express from 'express'
-
+import { faker } from '@faker-js/faker'
+import exphbs from 'express-handlebars'
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+import path from 'path'
+const url = import.meta.url
+const __filename = fileURLToPath(url)
+const __dirname = dirname(__filename)
+const hbs = exphbs.create({
+    defaultLayout: 'main',
+    extname: 'hbs'
+})
 
 const app = express()
 
-const PORT = process.env.PORT || 80
+const PORT = process.env.PORT || 8000
+
+app.use(express.static('app'))
 
 app.get('/', (req, res) => {
-    res.end('<h1>Hello</h1>')
+    res.status(200)
+    res.sendFile(path.join(__dirname, 'views', 'index.html'))
 })
 
 app.listen( PORT, () => {
